fcbc9dd9186bab57bc71e14c0d7626a9aa573ebe
IngrateSihombing <ingratejoy@gmail.com>
2024-10-18 11:54:35 +0700

    PAIR21

--- M01.java
+++ M01.java
@@ -7,55 +7,47 @@ public class M01 {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int totalkredit, indeks;
-        double performa, indekssementara, performaakhir, nilaiangka, a, aB, b, bC, c, d, e;
-        double kredit1;
+        String nilaiHuruf, kredit;
+        double nilaiAngka, performa;
 
-        kredit1 = 0;
-        String nilaihuruf, kredit;
-
-        totalkredit = 0;
-        kredit = input.nextLine();
-        while (!kredit.equals("---")) {
+        do {
+            kredit = input.nextLine();
             if (kredit.equals("---")) {
-                kredit = kredit;
             } else {
-                kredit1 = Integer.parseDouble(kredit);
-            }
-            nilaihuruf = input.nextLine();
-            if (nilaihuruf.equals("A")) {
-                indekssementara = 4.0;
-            } else {
-                if (nilaihuruf.equals("AB")) {
-                    indekssementara = 3.5;
+                nilaiHuruf = input.nextLine();
+                if (nilaiHuruf.equals("A")) {
+                    nilaiAngka = 4.0;
                 } else {
-                    if (nilaihuruf.equals("B")) {
-                        indekssementara = 3.0;
+                    if (nilaiHuruf.equals("AB")) {
+                        nilaiAngka = 3.5;
                     } else {
-                        if (nilaihuruf.equals("BC")) {
-                            indekssementara = 2.5;
+                        if (nilaiHuruf.equals("B")) {
+                            nilaiAngka = 3.0;
                         } else {
-                            if (nilaihuruf.equals("C")) {
-                                indekssementara = 2.0;
+                            if (nilaiHuruf.equals("BC")) {
+                                nilaiAngka = 2.5;
                             } else {
-                                if (nilaihuruf.equals("D")) {
-                                    indekssementara = 1.0;
+                                if (nilaiHuruf.equals("C")) {
+                                    nilaiAngka = 2.0;
                                 } else {
-                                    if (nilaihuruf.equals("E")) {
-                                        indekssementara = 0.0;
+                                    if (nilaiHuruf.equals("D")) {
+                                        nilaiAngka = 1.0;
                                     } else {
-                                        indekssementara = "";
+                                        if (nilaiHuruf.equals("E")) {
+                                            nilaiAngka = 0.0;
+                                        } else {
+                                            nilaiAngka = 0.0;
+                                        }
                                     }
                                 }
                             }
                         }
                     }
                 }
+                performa = Integer.parseInt(kredit) * nilaiAngka;
+                System.out.println(toFixed(performa,1));
             }
-            performaakhir = Integer.parseDouble(kredit) * indekssementara;
-            System.out.println(toFixed(performaakhir,1));
-            kredit = input.nextLine();
-        }
+        } while (!kredit.equals("---"));
     }
     
     private static String toFixed(double value, int digits) {

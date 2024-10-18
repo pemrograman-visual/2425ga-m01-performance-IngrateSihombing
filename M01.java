d5d8870b1ed9e27cab8082ba8898f231511d466f
IngrateSihombing <ingratejoy@gmail.com>
2024-10-18 10:29:24 +0700

    PAIR-21

--- M01.java
+++ M01.java
@@ -1,2 +1,65 @@
-// NIM - Your Name
-// NIM - Your Name
+//12S24022-Ingrate Joy Sihombing
+//12S24015-LucasPardede
+
+import java.util.*;
+import java.lang.Math;
+
+public class M01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int totalcredit, index;
+        double performance, temporaryindex, lastperformance, numbervalue, a, aB, b, bC, c, d, e;
+        double credit1;
+
+        credit1 = 0;
+        String lettergrade, credit;
+
+        totalcredit = 0;
+        credit = input.nextLine();
+        while (!credit.equals("---")) {
+            if (credit.equals("---")) {
+                credit = credit;
+            } else {
+                credit1 = Integer.parseDouble(credit);
+            }
+            lettergrade = input.nextLine();
+            if (lettergrade.equals("A")) {
+                temporaryindex = 4.0;
+            } else {
+                if (lettergrade.equals("AB")) {
+                    temporaryindex = 3.5;
+                } else {
+                    if (lettergrade.equals("B")) {
+                        temporaryindex = 3.0;
+                    } else {
+                        if (lettergrade.equals("BC")) {
+                            temporaryindex = 2.5;
+                        } else {
+                            if (lettergrade.equals("C")) {
+                                temporaryindex = 2.0;
+                            } else {
+                                if (lettergrade.equals("D")) {
+                                    temporaryindex = 1.0;
+                                } else {
+                                    if (lettergrade.equals("E")) {
+                                        temporaryindex = 0.0;
+                                    } else {
+                                        temporaryindex = 0.0;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            lastperformance = Integer.parseDouble(credit) * temporaryindex;
+            System.out.println(toFixed(lastperformance,1));
+            credit = input.nextLine();
+        }
+    }
+    
+    private static String toFixed(double value, int digits) {
+        return String.format("%." + digits + "f", value);
+    }
+}
